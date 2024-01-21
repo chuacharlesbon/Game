33de33cc7e386abe7a9eb6eb166a95d27b2595e7
CHARLES BON CHUA <101096185+chuacharlesbon@users.noreply.github.com>
2024-01-21 13:24:13 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,15 @@ export default function Home(){
 		<div className='w-full h-full flex items-center justify-center bg-secondary-200'>
 		  <img alt='sf' className='object-contain' src='https://i0.wp.com/www.reimarufiles.com/wp-content/uploads/2017/01/special-force-character.png?resize=540%2C389' />
 		  <p className='hidden'>{test}</p>
+		<div class="flex flex-row justify-center">
+		    <ins class="adsbygoogle"
+		         style="display:block; width: 300px;"
+		         data-ad-client="ca-pub-8167368561700289"
+		         data-ad-slot="3951014374"
+		         data-ad-format="auto"
+		         data-full-width-responsive="true">
+		    </ins>
+		</div>
 		</div>
 	</>
 
